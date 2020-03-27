76c2c525598f15b414663a5faed9d9c9d39a1110
Hecktor Viegas do Nascimento <hecktorvn@hotmail.com>
2020-03-27 10:38:24 -0300

    Back-end da aplicação

---
+++ backend/src/database/connection.js
@@ -0,0 +1,5 @@
+const knex = require('knex');
+const configurations = require('../../knexfile');
+
+const connection = knex( configurations.development );
+module.exports = connection;
---
+++ backend/src/routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/Listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no bakc-end
+ *
+ */
+routes.post('/session', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
